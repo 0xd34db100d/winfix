511046db3ec11e478076f0d410d9fc17e2947f70
Exitiale <54123032+Exitiale@users.noreply.github.com>
2023-04-29 02:20:41 +0500

    GUI Added

--- Winfix.py
+++ Winfix.py
@@ -1,24 +1,14 @@
 ﻿from logging import exception
 import os
-import signal
 import sys
 from ctypes import windll
-from time import sleep
 import psutil
-import readchar
-
-windll.kernel32.SetConsoleTitleW("Winfix")
-
-
-print('WinFix is a small script for fixing annoying Windows 10 bugs and errors. © John Strider 2022\nCurrent version: 1.0\n')
-
+import customtkinter
+import tkinter
+windll.kernel32.SetConsoleTitleW("Winfix v1.1")
 
 def internet():
-    os.system("ipconfig /flushdns")
-    os.system("ipconfig /release")
-    os.system("ipconfig /renew")
-    os.system("netsh winsock reset")
-
+    os.system("ipconfig /flushdns & ipconfig /release & ipconfig /renew & netsh winsock reset")
 
 def clipboard():
     os.system("dir | clip")
@@ -26,12 +16,69 @@ def clipboard():
         windll.user32.EmptyClipboard()
         windll.user32.CloseClipboard()
 
-
 def explorer():
     for proc in psutil.process_iter():
         if proc.name() == "explorer.exe":
             proc.kill()
 
+def gui():
+    print("Running gui mode! Type: 'Winfix list' or 'Winfix -l' to show cmd options.\nWARNING! YOU WILL SEE ALL COMMANDS OUTPUT, THERE CAN BE YOUR MAC ADDRESSES AND IP`s")
+    root = customtkinter.CTk()
+    try:
+        path_to_icon = os.path.abspath(os.path.join(os.path.dirname(__file__), 'icon/gear.ico')) 
+        root.iconbitmap(path_to_icon)
+    except:
+        pass
+    customtkinter.set_appearance_mode("System")
+    customtkinter.set_default_color_theme("green")
+    root.title("Winfix v1.1")
+    root.geometry("300x400")
+    root.resizable(False, False)
+
+    def guiclip():
+        try:
+            clipboard()
+        except Exception:
+            termf.insert(tkinter.END, "[Clipboard] Error!\n")
+            print("[Clipboard] Error!\n")
+        termf.insert(tkinter.END, "[Clipboard] Applied!\n")
+        print("[Clipboard] Applied!\n")
+
+    def guiinternet():
+        try:
+            internet()
+        except Exception:
+            termf.insert(tkinter.END, "[Internet] Error!\n")
+            print("[Internet] Error!\n")
+        termf.insert(tkinter.END, "[Internet] Applied! I recommend restarting PC.\n")
+        print("[Internet] Applied! I recommend restarting PC.\n")
+
+    def guiexplorer():
+        try:
+            explorer()
+        except Exception:
+            termf.insert(tkinter.END, "[Explorer] Error!\n")
+            print("[Explorer] Error!\n")
+        termf.insert(tkinter.END, "[Explorer] Applied!\n")
+        print("[Explorer] Applied!\n")
+
+    label = customtkinter.CTkLabel(master=root, text="Welcome to winfix!\nJust click what you want to apply :3\nMade by Exitiale#1024")
+    label.pack(pady=10, padx=10)
+
+    clipboardbtn = customtkinter.CTkButton(master=root, text="Clipboard fix", command=guiclip)
+    clipboardbtn.pack(pady=10, padx=10)
+
+    internetbtn = customtkinter.CTkButton(master=root, text="Internet fix", command=guiinternet)
+    internetbtn.pack(pady=10, padx=10)
+
+    explorerbtn = customtkinter.CTkButton(master=root, text="Explorer fix", command=guiexplorer)
+    explorerbtn.pack(pady=10, padx=10)
+
+    termf = customtkinter.CTkTextbox(root, height=250, width=300)
+    termf.bind("<Key>", lambda e: "break")
+    termf.pack(pady=10, padx=10)
+
+    root.mainloop()
 
 try:
     if (sys.argv[1] == "list" or sys.argv[1] == "-l"):
@@ -50,9 +97,8 @@ try:
         explorer()
         print("\nExplorer fix was applied successfully!")
     else:
-        print('OH, looks like there is some problem with input or error :<\n\nPlease, write "winfix.exe list" or "winfix.exe -l" to print available fixes. :3 \n')
+        print("Can't find arg. Running gui...")
+        gui()
 except:
-    print('OH, looks like there is some problem with input or error :<\n\nPlease, write "winfix.exe list" or "winfix.exe -l" to print available fixes. :3 \n')
+    gui()
 
-print("\nPress any key to exit...")
-k = readchar.readchar()
